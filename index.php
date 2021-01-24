7a48aafea2099496180a67c2d8eb6296f30e5f64
Phanquan12 <77915849+Phanquan12@users.noreply.github.com>
2021-01-24 15:57:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <tittle>
 Phan Minh Quan
-<img>https://user-images.githubusercontent.com/77915849/105625520-7a7dd080-5e5c-11eb-8848-abea4f0bae73.png</img>
+<img src="https://user-images.githubusercontent.com/77915849/105625520-7a7dd080-5e5c-11eb-8848-abea4f0bae73.png"/>
 </tittle>
 </head>
 </html>
